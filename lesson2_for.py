8462e1cbc87d6cfadc82d256bf944d194b2da733
templinsrepo <lolapaluza12123@gmail.com>
2023-10-23 12:21:31 +0300

    lesson21

---
+++ lesson2_for.py
@@ -0,0 +1,18 @@
+for x in range(1, 21):
+
+    print("x=", x, "x2=", x*x)
+
+students = ["Вася", "Маша", "Таня", "Света", "Миша", "Коля", "Дима", "Данил", "Марина"]
+
+for student in students:
+    print(student)
+
+for i in range(0, len(students)):
+    print(students[i])
+for x in range (len(students)):
+    print(students[x])
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for n in num:
+    if n % 2 == 1:
+        print(n)
