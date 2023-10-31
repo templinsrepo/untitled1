ecc6a97ad9dcfda93ac30a52e7d0bfaefc5c4cfe
templinsrepo <lolapaluza12123@gmail.com>
2023-10-31 21:20:33 +0300

    lesson2task5

---
+++ lesson_2_task_5.py
@@ -0,0 +1,13 @@
+
+def month_to_season(season):
+
+        if season == 12 or season == 1 or season == 2:
+            print("Зима")
+        elif season == 3 or season == 4 or season == 5:
+            print("Весна")
+        elif season == 6 or season == 7 or season == 8:
+            print("Лето")
+        elif season == 9 or season == 10 or season == 11:
+            print("Осень")
+
+month_to_season(9)
